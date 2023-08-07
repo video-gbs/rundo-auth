4c73b33a155d1ef9510579fa9639a444b192b5a1
Miracle <k22227011@outlook.com>
2023-08-07 17:47:54 +0800

    增加Druid的拦截器支持

--- rundo-auth-rbac/src/main/java/com/runjian/rbac/config/DruidFilterConfig.java
+++ rundo-auth-rbac/src/main/java/com/runjian/rbac/config/DruidFilterConfig.java
@@ -1,25 +1,21 @@
 package com.runjian.rbac.config;
 
-import com.alibaba.druid.filter.Filter;
-import com.alibaba.druid.pool.DruidDataSource;
+
 import com.alibaba.druid.wall.WallConfig;
 import com.alibaba.druid.wall.WallFilter;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
+ * druid的拦截器配置
  * @author Miracle
  * @date 2023/8/7 17:32
  */
 @Configuration
 @AllArgsConstructor
-public class DruidConfig {
+public class DruidFilterConfig {
 
     @Bean
     public WallFilter wallFilter() {
