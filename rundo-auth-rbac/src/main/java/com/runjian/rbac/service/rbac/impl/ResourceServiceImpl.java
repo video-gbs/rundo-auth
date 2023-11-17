84988a22a0b2772a4ae9c0b715d736f98f0d9b6e
Miracle <k22227011@outlook.com>
2023-11-17 14:37:47 +0800

    增加资源删除时，若资源下拥有资源不可删除

--- rundo-auth-rbac/src/main/java/com/runjian/rbac/service/rbac/impl/ResourceServiceImpl.java
+++ rundo-auth-rbac/src/main/java/com/runjian/rbac/service/rbac/impl/ResourceServiceImpl.java
@@ -169,6 +169,7 @@ public class ResourceServiceImpl implements ResourceService {
             resourceInfoList.add(pResourceInfo);
             Set<Long> resourceIds = resourceInfoList.stream().filter(resourceInfo -> Objects.equals(resourceInfo.getResourceType(), ResourceType.CATALOGUE.getCode())).map(ResourceInfo::getId).collect(Collectors.toSet());
             if (resourceInfoList.size() > resourceIds.size()){
+                dataSourceTransactionManager.commit(transaction);
                 throw new BusinessException(BusinessErrorEnums.VALID_ILLEGAL_OPERATION, "删除失败，该节点下或者下级节点下存在资源");
             }
             funcResourceMapper.deleteAllByResourceIds(resourceIds);
